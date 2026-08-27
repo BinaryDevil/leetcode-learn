6531b0f5ce6ca1ddc54fcf7c19dcc2e9dafa6546
Terry Li <rainy9271@gmail.com>
2026-08-26 17:21:44 -0700

    128 & 238

---
+++ problems/array/238.ProductofArrayExceptSelf.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    let prefix = new Array(nums.length).fill(1);
+    let suffix = new Array(nums.length).fill(1);
+
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+
+    for (let i = nums.length - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+    }
+
+    return prefix.map((_, i) => prefix[i] * suffix[i]);
+};
